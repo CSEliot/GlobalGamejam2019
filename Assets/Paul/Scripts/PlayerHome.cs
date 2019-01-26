4977d547372f1f8527c4ebbe0397682df3c022fe
TSCHIRGIMACHINE\pault_000 <pault1@umbc.edu>
2019-01-25 21:22:58 -0500

    more files and some text basics

---
+++ Assets/Paul/Scripts/PlayerHome.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public enum PersonalityType
+{
+    Perfectionist, Hoarder, Nerd, Tacky, Cheap, Expensive, Hermit, Athletic
+}
+
+
+public class PlayerHome : MonoBehaviour
+{
+    public PersonalityType houseType;
+    public List<Room> allRooms = new List<Room>();
+
+    //public List<objItems> needs = new List<objItems>();
+
+    public void DropItemInRoom(int roomInt)
+    {
+
+    }
+}
