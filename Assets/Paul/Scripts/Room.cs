f74e2ec2913908fcfd2bcc16e29548e5ffb09fc9
TSCHIRGIMACHINE\pault_000 <7443112+ThinkingonPause@users.noreply.github.com>
2019-01-25 21:22:58 -0500

    more files and some text basics

---
+++ Assets/Paul/Scripts/Room.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class SlotSpace
+{
+    public Transform anchorSpot;
+}
+
+
+[System.Serializable]
+public class Room : MonoBehaviour
+{
+    public List<SlotSpace> slots = new List<SlotSpace>();
+
+
+    public void TakeThisObject()
+    {
+
+    }
+}
+
